04a2bde7d6adc87289c658f9a0ef973547ed4b38
renf7 <renf7@o2.pl>
2023-06-05 09:03:29 +0200

    deal with apply-bucardo-config.sh: Permission denied in Unix mother VM

--- docker-start-up.sh
+++ docker-start-up.sh
@@ -1,5 +1,5 @@
 # Call check bucardo.json in infinity loop in background
-while true; do sleep 10; apply-bucardo-config.sh;   done &
+while true; do sleep 10; /bin/bash apply-bucardo-config.sh;   done &
 
 # Call entrypoint and cmd of the postgres docker image
 # It is blocking command, so it makes container to run permanently
